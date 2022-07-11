8c332cd8356213c97d0df242aef58ac9c1b93172
rahulchaudhary777 <ch.rahuljaat777@gmail.com>
2022-07-11 19:23:48 +0530

    Assignment

---
+++ Template-1.js
@@ -0,0 +1,13 @@
+var n = document.getElementById("n");
+var roll = document.getElementById("roll");
+var date = document.getElementById("date");
+
+n.innerHTML = "Rahul";
+roll.innerHTML = "201500542";
+
+var day = new Date();
+var d = day.getDate();
+var month = day.getMonth();
+var year = day.getFullYear();
+
+date.innerHTML = `${d}-${month}-${year}`;
